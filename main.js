07c206ac8185671078d36b8c51adce986e593abe
Coskun Uyar <coskunuyarr@gmail.com>
2020-03-30 17:36:00 +0300

    Add window is added.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { app , BrowserWindow , Menu} = electron;
 
 let mainWindow;
-
+let addWindow;
 
 // Create menu template
 const mainMenuTemplate = [
@@ -13,13 +13,18 @@ const mainMenuTemplate = [
         label: 'File',
         submenu: [
             {
-                label: 'Add Item'
+                label: 'Add Item',
+                click(){
+                    createAddWindow();
+                },
+
             },
             {
                 label: 'Clear Items'
             },
             {
                 label: 'Quit',
+                accelerator: process.platform === 'darwin'  ? 'Command+Q' : 'Ctrl+Q',
                 click(){
                     app.quit();
                 }
@@ -27,6 +32,27 @@ const mainMenuTemplate = [
         ]
     }
 ];
+
+// Handle create add window
+function createAddWindow(){
+    // Create new window.
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Shopping List Item'
+    });
+    // Load html into window.
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname,'addWindow.html'),
+        protocol:'file:',
+        slashes: true
+    }));
+
+    // Garbage collection handle
+    addWindow.on('close',() => {
+        addWindow = null;
+    });
+}
 
 // Listen for app to be ready
 app.on('ready',() => {
@@ -38,6 +64,11 @@ app.on('ready',() => {
         protocol:'file:',
         slashes: true
     }));
+
+    // Quit app when closed
+    mainWindow.on('closed',() => {
+        app.quit();
+    })
 
     // Buil menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
